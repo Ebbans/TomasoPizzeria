33f2229780591bef76a7e7d29ba5db4b152137ff
ebru mermer <ebrumermer@live.se>
2025-05-26 22:35:22 +0200

    new changes

--- obj/Debug/net8.0/Inlämning1Tomaso.AssemblyInfo.cs
+++ obj/Debug/net8.0/Inlämning1Tomaso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inlämning1Tomaso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f215cc72bea21c3d8883bfa38ae6877f705332b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab81346ca9d0d11d030db992cfa80fa0e90ee20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inlämning1Tomaso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inlämning1Tomaso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
