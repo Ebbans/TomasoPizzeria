cd32a20eb610b35401706aca12025d30c7ab89ec
ebru mermer <ebrumermer@live.se>
2025-06-03 11:08:55 +0200

    final changes

--- Data/DTOs/OrderDto.cs
+++ Data/DTOs/OrderDto.cs
@@ -6,5 +6,5 @@ public class OrderDto
     public string UserName { get; set; }
     public DateTime OrderDate { get; set; }
     public decimal Price { get; set; }
-    public List<OrderDishDto> Dishes { get; set; }  // valfritt men bra
+    public List<OrderDishDto> Dishes { get; set; }  
 }
